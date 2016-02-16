bd35094a96de3c9f92f5f02f0942bf7196ba54bf
Harekam Singh <harekamsingh@clicklabs32s-MacBook-Air.local>
2016-02-16 22:54:10 +0530

    react forms implementation

---
+++ src/components/authors/manageAuthorPage.js
@@ -0,0 +1,14 @@
+/**
+ * Created by harekamsingh on 2/16/16.
+ */
+"use strict";
+var React = require('react');
+var ManageAuthorPage = React.createClass({
+    render: function () {
+        return (
+            <h1>Manage Author</h1>
+        );
+    }
+});
+
+module.exports = ManageAuthorPage;
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,13 +3,15 @@
  */
 'use strict';
 var React = require('react');
-
+var Router = require('react-router');
+var Link = Router.Link;
 var Home = React.createClass({
     render: function () {
         return (
             <div className="jumbotron">
                 <h1>Admin Panel</h1>
                 <p>Using react, react router, flux.</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
             </div>
         );
     }
